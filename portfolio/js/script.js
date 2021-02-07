36229d717eba5a177f7018bc1bb28f8fc94412fe
Kohei <18ec092@ms.dendai.ac.jp>
2021-02-07 14:45:29 +0900

    Create portfolio

---
+++ portfolio/js/script.js
@@ -0,0 +1,45 @@
+// PhotoSwipe
+initPhotoSwipeFromDOM(".js-my-gallery");
+
+$(function () {
+
+  //iOS対策
+  var ua = navigator.userAgent;
+  if (/iPhone|iPad|iPod/.test(ua)) {
+    $("body").addClass("ios");
+  }
+
+  //Worksのリンクを有効化
+  $(".works-url").on("click", "a", function (e) {
+    e.stopPropagation();
+  });
+
+  //ページ内スクロール
+  var $nav = $(".gnav");
+  var navHeight = $nav.outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate(
+      {
+        scrollTop: position,
+      },
+      300,
+      "swing"
+    );
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      300
+    );
+    return false;
+  });
+});
